cb3beccd16d476c4b6eb8f2fbaf7979a795b823a
Pratyush Verma <vermapratyush@gmail.com>
2015-06-01 17:17:25 +0530

    removed unused code

--- src/chatter/main.go
+++ src/chatter/main.go
@@ -4,55 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"io/ioutil"
-	"github.com/satori/uuid"
 	"net/url"
 	"chatter/sock"
 	"golang.org/x/net/websocket"
 )
 
-var server = sock.NewServer()
 var pp = sock.Init()
 var index = 0
-
-func submitNewPage(ws *websocket.Conn) {
-	body, err := ioutil.ReadAll(ws.Request().Body)
-	fmt.Printf("%+v", string(body))
-	if err != nil {
-		fmt.Printf("Unable to read: %s", err)
-		ws.Write([]byte("Unable to read"))
-	} else {
-		fmt.Printf("Body = %s <end>\n", string(body))
-		fmt.Print(string(body))
-		id := uuid.NewV1().String()
-		c := sock.NewClient(ws, server, id)
-		c.Write("test response")
-//		go c.Listen()
-		_, err := server.AddNewClient(id, c)
-
-		if err != nil {
-			fmt.Printf("Can not add client to the server %s", err)
-		} else {
-			newUrl := fmt.Sprintf("/sha?id=%s", id)
-			fmt.Printf("Go to %s\n", newUrl)
-		}
-	}
-
-}
-
-func openNewWebSocket(ws *websocket.Conn) {
-	uri, _ := url.Parse(ws.Request().RequestURI)
-	id, present := uri.Query()["id"]
-	if present == false || len(id) != 1 {
-		ws.Write([]byte("Bad Request"))
-	} else {
-		c := sock.NewClient(ws, server, id[0])
-		ws.Write([]byte(c.Document))
-	}
-}
-func createPage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "webroot/createPage.html")
-}
 
 func debug(w http.ResponseWriter, r *http.Request) {
 	uri, _ := url.ParseRequestURI(r.RequestURI)
@@ -68,12 +26,10 @@ func debug(w http.ResponseWriter, r *http.Request) {
 func main() {
 	go pp.Listen()
 	http.HandleFunc("/debug", debug)
-//	http.Handle("/sha", websocket.Handler(openNewWebSocket))
 	http.Handle("/p2pAdd", websocket.Handler(p2pAddSocket))
 	http.Handle("/p2pGet", websocket.Handler(p2pGetSocket))
 	http.Handle("/", http.FileServer(http.Dir("webroot")))
 
-//	http.HandleFunc("/createPage", createPage)
 	log.Fatal(http.ListenAndServe(":22222", nil))
 
 }
